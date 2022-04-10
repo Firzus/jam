51ee1610ec9cb2f3bf8f0ab3a918bb0646a2b09e
unknown <lprieu@gamingcampus.local>
2022-04-10 15:27:29 +0200

    ok

--- website/js/script.js
+++ website/js/script.js
@@ -19,3 +19,14 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+// Download Button
+
+function btn_active(){
+  document.querySelector('.texto_centro').className = "texto_centro active_txt";
+    document.querySelector('.cont_centrar').className = "cont_centrar activebtn";
+    setTimeout(function(){
+        document.querySelector('.cont_centrar').className = "cont_centrar activebtn_fin";
+      document.querySelector('.texto_centro').className = "texto_centro op_0";
+    },15000);
+  }
